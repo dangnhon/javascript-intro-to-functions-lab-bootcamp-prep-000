c99886e7aeb9958ea13e2aa95bb3d081c314b670
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-28 05:00:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ mixcase.toLowerCase() === mixcase;
 function sayHiToGrandma(string) {
   if (lowercase) { return("I can't hear you!"); }}
   
-function sayHiToGrandma(string) {
-  if (uppercase) { return("YES INDEED!"); }}
+
   
 
 
